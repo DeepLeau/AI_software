d3f0a51e2f8ada96437ebae6deea57061540d3be
DeepLeau <thomas.bodenan@gmail.com>
2024-10-03 16:05:20 +0200

    final version

--- nvemo.py
+++ nvemo.py
@@ -1,6 +1,9 @@
 import pyttsx3
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from diffusers import StableDiffusionPipeline
+import torch
+
 
 # Initialiser le modèle de chatbot
 template = """
@@ -14,6 +17,7 @@ Answer:
 """
 
 model = OllamaLLM(model="llama3")
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4").to("cuda" if torch.cuda.is_available() else "cpu")
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
@@ -27,3 +31,16 @@ def process_question(context: str, question: str) -> str:
 #    engine = pyttsx3.init()
 #    engine.say(text)
 #    engine.runAndWait()
+
+
+def summarize_text(text: str)->str:
+    question = "Can you summarize the following text ? Just provide the text nothing more, nothing less."
+    context = f"Text: {text}"
+    summary = process_question(context, question)
+    return summary
+
+def generate_image(prompt: str):
+    """Generates an image from a text prompt."""
+    image = pipe(prompt).images[0]
+    image.save("generated_image.png")
+    return "generated_image.png"
--- nvemo_api.py
+++ nvemo_api.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from nvemo import process_question
+from nvemo import process_question, summarize_text, generate_image
+import pdfplumber
+
 
 app = FastAPI()
 
@@ -18,11 +20,37 @@ class QuestionRequest(BaseModel):
     context: str
     question: str
 
+class ImageRequest(BaseModel):
+    prompt: str
+
 @app.post("/chat/")
 async def chat(request: QuestionRequest):
     response = process_question(request.context, request.question)
     #speak(response)
     return {"response": response}
+
+@app.post("/upload-pdf/")
+async def upload_pdf(file: UploadFile = File(...)):
+    pdf_text = extract_text_from_pdf(file)
+
+    summary = summarize_text(pdf_text)
+    print(summary)
+    return {"summary": summary}
+
+def extract_text_from_pdf(uploaded_file):
+    with pdfplumber.open(uploaded_file.file) as pdf:
+        full_text = ""
+        for page in pdf.pages:
+            full_text += page.extract_text()
+    return full_text
+
+@app.post("/generate-image/")
+async def generate_image_api(request: ImageRequest):
+    try:
+        image_url = generate_image(request.prompt)
+        return {"image_url": f"http://127.0.0.1:8000/image-generation/{image_url}"}
+    except Exception as e:
+        return {"error": str(e)}
 
 if __name__ == "__main__":
     import uvicorn
